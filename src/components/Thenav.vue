36fd02a79c3114483f0b2ce3f0c727dc401ea3ea
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-07 21:17:11 +0700

    1 file modified

--- src/components/Thenav.vue
+++ src/components/Thenav.vue
@@ -6,15 +6,15 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Thenav',
   props: {
     msg: String
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+
+<style>
 h3 {
   margin: 40px 0 0;
 }
